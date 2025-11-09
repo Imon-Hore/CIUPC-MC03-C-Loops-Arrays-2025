4c32fb7b277d3cfdc7525d4da07aa10846389fa3
Imon-Hore <itshoreimonuni@gmail.com>
2025-11-09 20:23:09 +0600

    Add files via upload

--- 01_Loops/01.c
+++ 01_Loops/01.c
@@ -1,1 +1,18 @@
+/* C CODE TO PRINT THE MULTIPLICATION TABLE OF A NUMBER : */
+#include<stdio.h>
+int main()
+{
+    int number, i;
+    printf ("Enter the number : ");
+    scanf ("%d", &number);
+    for (i = 1; i <= 10; i++)
+    {
+        printf ("%d * %d = %d\n", number, i, number*i);
+    }
+    return 0;
+}
+
+
+
+
 
